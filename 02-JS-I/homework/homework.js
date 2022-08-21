3bacbec0b5f6bf382f2a27082da4f9a71b4acd7f
Edward <davidlopez0794@gmail.com>
2022-08-20 21:45:56 -0500

    update homework js 1, areadeltriangulo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -168,7 +168,7 @@ function redondearNumero(num) {
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  return match.ceil(num);
+  return Math.ceil(num);
 }
 
 function numeroRandom() {
@@ -182,47 +182,59 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (numero === 0) {
+    return false;
+  }
+  else if (numero > 0) {
+    return "Es positivo";
+  } else { 
+    return "Es negativo";
+}
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
-  // Tu código:
-  
+  // Tu código: 
+  var combinado = nombre + ' ' + apellido;
+  return combinado;
+
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+
+  return 'Hola ' + nombre + '!';
   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return alto * ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado * 4;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+return (base * altura)/2;
 }
 
 
